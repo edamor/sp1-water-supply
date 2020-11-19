0a03d67e212f211ccc09a35ffef827f20b010613
ed louise amor <ed.amor@xlog.com>
2020-11-20 01:48:33 +0800

    try kung mag fetch pa siya

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,7 +5,6 @@ const DOMAIN = "https://sp1-blue-sparrow.herokuapp.com/api/v1";
 export const useFetch = ({endpoint, token}) => {
   const [state, setState] = useState({data: null, loading: true});
   useEffect(() => {
-    console.log("NAG FETCH!");
     setState({data: null, loading: true});
     fetch(DOMAIN + endpoint, {
       method: "GET",
@@ -16,6 +15,7 @@ export const useFetch = ({endpoint, token}) => {
     .then(response => response.text())
     .then(x => {
       setState({data: JSON.parse(x), loading: false});
+      console.log("NAG FETCH!");
     })
     .catch(e => {
       alert(e)
