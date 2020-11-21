17e8445401b97fccb7129555b6c8129647d466c0
ed louise amor <ed.amor@xlog.com>
2020-11-22 00:15:18 +0800

    test if existing state

---
+++ src/components/CreateAccountButton/CreateAccountButton.js
@@ -0,0 +1,14 @@
+
+
+export const CreateAccountButton = () => {
+
+
+  return (
+    <button 
+      type="submit" 
+      className="btn btn-primary"
+    >
+      Submit
+    </button>
+  )
+}
--- src/pages/main/CreateAccount/CreateAccount.js
+++ src/pages/main/CreateAccount/CreateAccount.js
@@ -1,26 +1,58 @@
 import { useRef, useState } from "react";
+import { CreateAccountButton } from "../../../components/CreateAccountButton/CreateAccountButton";
 
 
 
 export const CreateAccount = () => {
 
+  // States
+  const [accountNumber, setAccountNumber] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [barangay, setBarangay] = useState("");
+  const [lastBillNumber, setLastBillNumber] = useState(0);
+  const [lastBillReading, setLastBillReading] = useState(0);
+  const [lastBillPeriodTo, setLastBillPeriodTo] = useState(0);
+  const [meterSerialNumber, setMeterSerialNumber] = useState("");
+  const [isExisting, setIsExisting] = useState(false);
+
+
+  // Refs
   let isExistingRef = useRef(true);
   let accountNumberRef = useRef(true);
+  let meterSerialNumberRef = useRef(true);
   let fullNameRef = useRef(true);
   let mobileNumberRef = useRef(true);
   let addressRef = useRef(true);
   let barangayRef = useRef(true);
 
-  const [isExisting, setIsExisting] = useState(false);
+  
 
-  const inputFields = [
+  const primaryInputFields = [
     {
       id: "accountNumber",
       label: "Account No.",
       placeholder: "ex. 2020-00-032",
       type: "text",
-      className: "col-md-6",
+      className: "col-md-4",
       ref: accountNumberRef
+    },
+    {
+      id: "meterSerialNumber",
+      label: "Meter Serial No.",
+      placeholder: "ex. EVJ 3324211",
+      type: "text",
+      className: "col-md-4",
+      ref: meterSerialNumberRef
+    },
+    {
+      id: "fullName",
+      label: "Full Name",
+      placeholder: "ex. Juan Santos",
+      type: "text",
+      className: "col-md-6",
+      ref: fullNameRef
     },
     {
       id: "mobileNumber",
@@ -29,14 +61,6 @@ export const CreateAccount = () => {
       type: "number",
       className: "col-md-6",
       ref: mobileNumberRef
-    },
-    {
-      id: "fullName",
-      label: "Full Name",
-      placeholder: "Juan Santos",
-      type: "text",
-      className: "col-md-12",
-      ref: fullNameRef
     },
     {
       id: "address",
@@ -48,11 +72,17 @@ export const CreateAccount = () => {
     }
   ];
 
-  const showInputFields = (x) => (
+  const showPrimaryInputFields = (x) => (
     x.map(item => (
-      <div className={item.className}>
+      <div className={item.className} key={item.id}>
         <label for={item.id} className="form-label">{item.label}</label>
-        <input type={item.type} className="form-control" id={item.id} ref={item.ref} />
+        <input 
+          type={item.type} 
+          className="form-control" 
+          id={item.id} 
+          ref={item.ref}
+          placeholder={item.placeholder}
+        />
       </div>
     ))
   );
@@ -83,7 +113,43 @@ export const CreateAccount = () => {
       label: "Camili",
       value: "CAM"
     },
+  ];
+
+
+  const lastBillInputFields = [
+    {
+      id: "lastBillNumber",
+      label: "Latest Bill No.",
+      placeholder: "ex. 56",
+      type: "number"
+    },
+    {
+      id: "lastBillReading",
+      label: "Latest Meter Reading",
+      placeholder: "ex. 56",
+      type: "number"
+    }
   ]
+
+  
+  const ifExisting = () => (
+    <div className="col-md-12">
+      {lastBillInputFields.map(item => (
+        <div className="col-md-6" key={item.id}>
+          <label for={item.id} class="form-label">
+            {item.label}
+          </label>
+          <input 
+            type={item.type} 
+            class="form-control" 
+            id={item.id} 
+            placeholder={item.placeholder}
+          />
+        </div>
+      ))}
+    </div>
+  )
+
 
 
   return (
@@ -92,19 +158,21 @@ export const CreateAccount = () => {
         New Account
       </p>
       <form className="row g-3">
-        <div className="col-md-12">
-          <p className="font-weight-bold">
-            Select if account is new or existing from old system
-          </p>
+        <div className="col-md-4">
+          <label className="form-check-label">
+            Select if account is New or Existing
+          </label>
           <div className="form-check form-check-inline">
             <input 
               className="form-check-input" 
               type="radio" 
               name="newOrExistingAccountOptions" 
-              id="newAccount" 
+              id="newAccount"
+              checked 
               value={false}
               onChange={(e) => {
                 console.log("New: \t" + e.target.value);
+                setIsExisting(false)
               }}
             />
             <label className="form-check-label" for="newAccount">New</label>
@@ -118,24 +186,28 @@ export const CreateAccount = () => {
               value={true}
               onChange={(e) => {
                 console.log("New: \t" + e.target.value);
+                setIsExisting(true)
               }}
             />
             <label className="form-check-label" for="existingAccount">Existing</label>
           </div>
         </div>
 
-        {showInputFields(inputFields)}
+        {showPrimaryInputFields(primaryInputFields)}
         
         <div className="col-md-4">
           <label for="inputBarangay" className="form-label">Barangay</label>
-          <select id="inputBarangay" className="form-select">
+          <select id="inputBarangay" className="form-select" ref={barangayRef}>
             <option selected disabled value="">Choose a barangay</option>
             {barangayList.map(item => <option value={item.value}>{item.label}</option>)}
           </select> 
         </div>
         
+        {isExisting && ifExisting()}
+
+
         <div className="col-12">
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <CreateAccountButton />
         </div>
       </form>
     </div>
