eca762d2f6382169ddedc34f058e01d92e8637d2
ed louise amor <ed.amor@xlog.com>
2020-11-21 21:51:32 +0800

    fix bills listing layout

--- src/components/BillListing/BillListing.js
+++ src/components/BillListing/BillListing.js
@@ -26,90 +26,73 @@ export const BillListing = ({bills, viewBill}) => {
   
 
   return (
-    <div className="list-group">
-    
-    <div className="container col-12 col-md-10 mx-auto">
-      <table className="table table-hover table-striped text-center">
-        <thead>
-          <tr>
-            <th scope="col">Bill No.</th>
-            <th scope="col">Period Covered</th>
-            <th scope="col">Consumption</th>
-            <th scope="col">Amount</th>
-            <th scope="col">
-              
-              
-              <select 
-                className="form-select" 
-                aria-label=".form-select-lg example"
-                onChange={(e) => {
-                  setSelectedYear(e.target.value)
-                }}
-              >
-                {years.map(item => <option value={item.value}>{item.name}</option>)}
-              </select>     
+    <table className="table table-hover table-striped text-center">
+      <thead>
+        <tr>
+          <th scope="col">Bill No.</th>
+          <th scope="col">Period Covered</th>
+          <th scope="col">Consumption</th>
+          <th scope="col">Amount</th>
+          <th scope="col">
+            
+            
+            <select 
+              className="form-select" 
+              aria-label=".form-select-lg example"
+              onChange={(e) => {
+                setSelectedYear(e.target.value)
+              }}
+            >
+              {years.map(item => <option value={item.value}>{item.name}</option>)}
+            </select>     
 
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            (results.length !== 0) ?
-              results.map(item => (
-                <tr key={item.billNumber}>
-                  <th scope="row">
-                    {item.billNumber}
-                  </th>
-                  <td>
-                    {
-                      `${new Date(item.periodFrom).toDateString().substring(4,7)}. ${new Date(item.periodFrom).toDateString().substring(8,10)} - 
-                      ${new Date(item.periodTo).toDateString().substring(4,7)}. ${new Date(item.periodTo).toDateString().substring(8,10)}`
-                    }
-                  </td>
-                  <td>
-                    {`${item.cumUsed} Cu. Meters` }
-                  </td>
-                  <td>
-                    {`PHP ${item.totalAmountDue}.00` }
-                  </td>
-                  <td className="d-flex justify-content-around">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-primary"
-                      onClick={(e) => {viewBill(e.target.value)}}
-                      value={item.billNumber}
-                    >
-                      View Details
-                    </button>
-                  </td>
-                </tr>
-              ))
-              :
-              (
-                <tr>
-                  <td colSpan={5} className="py-4">
-                    <em>
-                      No Available Records for {new Date(selectedYear).toDateString()}
-                    </em>
-                  </td>
-                </tr>
-              )
-          }
-
-        </tbody>
-      </table>    
-    </div>
-    </div>    
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          (results.length !== 0) ? results.map(item => (
+            <tr key={item.billNumber}>
+              <th scope="row">
+                {item.billNumber}
+              </th>
+              <td>
+                {
+                  `${new Date(item.periodFrom).toDateString().substring(4,7)}. ${new Date(item.periodFrom).toDateString().substring(8,10)} - 
+                  ${new Date(item.periodTo).toDateString().substring(4,7)}. ${new Date(item.periodTo).toDateString().substring(8,10)}`
+                }
+              </td>
+              <td>
+                {`${item.cumUsed} Cu. Meters` }
+              </td>
+              <td>
+                {`PHP ${item.totalAmountDue}.00` }
+              </td>
+              <td className="d-flex justify-content-around">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-primary"
+                  onClick={(e) => {viewBill(e.target.value)}}
+                  value={item.billNumber}
+                >
+                  View Details
+                </button>
+              </td>
+            </tr>
+          ))
+          :
+          (
+            <tr>
+              <td colSpan={5} className="py-4">
+                <em>
+                  No Available Records for {new Date(selectedYear).toDateString()}
+                </em>
+              </td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>      
   )
 }
-
-
-
-      // <button 
-      //   type="button" 
-      //   className="list-group-item list-group-item-action active" 
-      //   aria-current="true"
-      // >
-      //   Cras justo odio
-      // </button>
      
