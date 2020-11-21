1d5b51b97b2dccf8cf71fd1acecaabfac049e841
ed louise amor <ed.amor@xlog.com>
2020-11-21 22:26:44 +0800

    include first draft of create account page

--- src/pages/main/CreateAccount/CreateAccount.js
+++ src/pages/main/CreateAccount/CreateAccount.js
@@ -3,50 +3,117 @@
 
 export const CreateAccount = () => {
 
+  const inputFields = [
+    {
+      id: "accountNumber",
+      label: "Account No.",
+      placeholder: "ex. 2020-00-032",
+      type: "text",
+      className: "col-md-6"
+    },
+    {
+      id: "mobileNumber",
+      label: "Mobile No.",
+      placeholder: "ex. 9278329101",
+      type: "number",
+      className: "col-md-6"
+    },
+    {
+      id: "fullName",
+      label: "Full Name",
+      placeholder: "Juan Santos",
+      type: "text",
+      className: "col-md-12"
+    },
+    {
+      id: "address",
+      label: "Address",
+      placeholder: "ex. Pob., Alcantara, Romblon",
+      type: "text",
+      className: "col-md-8"
+    }
+  ];
+
+  const showInputFields = (x) => (
+    x.map(item => (
+      <div className={item.className}>
+        <label for={item.id} className="form-label">{item.label}</label>
+        <input type={item.type} className="form-control" id={item.id} />
+      </div>
+    ))
+  );
+
+
+  const barangayList = [
+    {
+      label: "Poblacion",
+      value: "POB"
+    },
+    {
+      label: "Gui-ob",
+      value: "GUI"
+    },
+    {
+      label: "Madalag",
+      value: "MAD"
+    },
+    {
+      label: "Bagsik",
+      value: "BAG"
+    },
+    {
+      label: "Lawan",
+      value: "LAW"
+    },
+    {
+      label: "Camili",
+      value: "CAM"
+    },
+  ]
+
+
   return (
     <div className="container">
+      <p className="display-6 text-center">
+        New Account
+      </p>
       <form className="row g-3">
-        <div className="col-md-6">
-          <label for="inputEmail4" className="form-label">Email</label>
-          <input type="email" className="form-control" id="inputEmail4" />
-        </div>
-        <div className="col-md-6">
-          <label for="inputPassword4" className="form-label">Password</label>
-          <input type="password" className="form-control" id="inputPassword4" />
-        </div>
-        <div className="col-12">
-          <label for="inputAddress" className="form-label">Address</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-        </div>
-        <div className="col-12">
-          <label for="inputAddress2" className="form-label">Address 2</label>
-          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-        </div>
-        <div className="col-md-6">
-          <label for="inputCity" className="form-label">City</label>
-          <input type="text" className="form-control" id="inputCity" />
-        </div>
+
+        {showInputFields(inputFields)}
+        
         <div className="col-md-4">
-          <label for="inputState" className="form-label">State</label>
-          <select id="inputState" className="form-select">
-            <option selected>Choose...</option>
-            <option>...</option>
-          </select>
+          <label for="inputBarangay" className="form-label">Barangay</label>
+          <select id="inputBarangay" className="form-select">
+            <option selected disabled value="">Choose a barangay</option>
+            {barangayList.map(item => <option value={item.value}>{item.label}</option>)}
+          </select> 
         </div>
-        <div className="col-md-2">
-          <label for="inputZip" className="form-label">Zip</label>
-          <input type="text" className="form-control" id="inputZip" />
-        </div>
-        <div className="col-12">
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" id="gridCheck" />
-            <label className="form-check-label" for="gridCheck">
-              Check me out
-            </label>
+        <div className="col-md-12">
+          <p>Select if account is new or existing from old system</p>
+          <div className="form-check form-check-inline">
+            <input 
+              className="form-check-input" 
+              type="radio" 
+              name="newOrExistingAccountOptions" 
+              id="newAccount" 
+              value={false}
+            />
+            <label className="form-check-label" for="newAccount">New</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input 
+            className="form-check-input" 
+            type="radio" 
+            name="newOrExistingAccountOptions" 
+            id="existingAccount" 
+            value={true} 
+          />
+            <label className="form-check-label" for="existingAccount">Existing</label>
           </div>
         </div>
+        
         <div className="col-12">
-          <button type="submit" className="btn btn-primary">Sign in</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </div>
       </form>
     </div>
